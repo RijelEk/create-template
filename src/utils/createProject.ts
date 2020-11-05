6b2efaf3cf5ad0b3cef2bd89bdff91e7d04d690c
Jaroslaw <yaroslav@xuver-services.com>
2020-11-05 17:11:42 +0200

    Create project folder function

---
+++ src/utils/createProject.ts
@@ -0,0 +1,12 @@
+import * as fs from 'fs';
+import chalk from 'chalk';
+
+export default function createProject(projectPath: string) {
+    if (fs.existsSync(projectPath)) {
+        console.log(chalk.red(`Folder ${projectPath} exists. Delete or use another name.`));
+        return false;
+    }
+    fs.mkdirSync(projectPath);
+    
+    return true;
+}
