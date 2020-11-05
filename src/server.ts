cd98853130ba337f43d12a64b27bf67b30aa58eb
Jaroslaw <yaroslav@xuver-services.com>
2020-11-05 16:30:31 +0200

    First commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import 'module-alias/register';
+import express from "express";
+
+const server = async () => {
+
+  const app = express();
+
+  const PORT = process.env.PORT || 3007;
+  app.listen(PORT, () => {
+    console.log(`Server started on localhost:${PORT}`);
+  });
+};
+
+server().catch((err) => {
+  console.error(err);
+});
