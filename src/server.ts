1878e614d53db4cef2d464a8ea0cbac4265bfe59
Jaroslaw <yaroslav@xuver-services.com>
2020-11-05 16:44:31 +0200

    Configure first questions

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,31 @@
 #!/usr/bin/env node
+import * as inquirer from 'inquirer';
+import chalk from 'chalk';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as yargs from 'yargs';
 
-import 'module-alias/register';
-import express from "express";
+const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'));
+const QUESTIONS = [
+    {
+        name: 'template',
+        type: 'list',
+        message: 'What stack would you prefer?',
+        choices: CHOICES
+    },
+    {
+        name: 'name',
+        type: 'input',
+        message: 'Project name:',
+        when: () => !yargs.argv['name'],
+        validate: (input: string) => {
+          if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+          else return 'Project name may only include letters, numbers, underscores and hashes.';
+        }
+      }
+];
 
-const server = async () => {
-
-  const app = express();
-
-  const PORT = process.env.PORT || 3007;
-  app.listen(PORT, () => {
-    console.log(`Server started on localhost:${PORT}`);
-  });
-};
-
-server().catch((err) => {
-  console.error(err);
+inquirer.prompt(QUESTIONS)
+.then(answers => {
+    console.log(answers);
 });
